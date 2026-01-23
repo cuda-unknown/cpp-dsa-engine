8ae11b4aa92c579b636afb0022f60ada52234621
cuda-unknown <soonyouwillknow34@gmail.com>
2026-01-23 10:24:17 +0530

    Feat: Added Recursive Binary Search, Array Sum, and Linear Search

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
     cout << "--- DSA Engine: Logic Testing ---" << endl;
-
+    
     // Test 1: Anagram
     if (isAnagram("anagram", "nagaram")) {
         cout << "[PASS] Anagram Logic Correct" << endl;
@@ -18,6 +18,34 @@ int main() {
         cout << "[PASS] Ransom Note Logic Correct" << endl;
     } else {
         cout << "[FAIL] Ransom Note Logic Error" << endl;
+    }
+
+    int arr[] = {2, 4, 6, 10, 14, 18};
+    int size = 6;
+    int key = 10;
+
+    if(binarySearch(arr, 0, size - 1, key)) {
+        cout << "Binary Search: Element " << key << " FOUND!" << endl;
+    } else {
+        cout << "Binary Search: Element " << key << " NOT found." << endl;
+    }
+
+    // 2. Linear Search
+    if(linearSearch(arr, size, key)) {
+        cout << "Linear Search: Element " << key << " FOUND!" << endl;
+    } else {
+        cout << "Linear Search: Element " << key << " NOT found." << endl;
+    }
+
+    //3. getsum
+    int totalSum = getSum(arr, size);
+    cout << "Sum of Array: " << totalSum << endl;
+
+    // 4. isSorted
+    if(isSorted(arr, size)) {
+        cout << "Array Status: The array is SORTED." << endl;
+    } else {
+        cout << "Array Status: The array is NOT sorted." << endl;
     }
 
     return 0;
--- solver.h
+++ solver.h
@@ -4,7 +4,6 @@
 #include <string>
 using namespace std;
 
-// No class needed here, just the logic
 bool isAnagram(string s, string t) {
     if(s.length() != t.length()) return false;
     int count[26] = {0};
@@ -19,12 +18,65 @@ bool isAnagram(string s, string t) {
 bool canConstruct(string ransomNote, string magazine) {
     if(ransomNote.length() > magazine.length()) return false;
     int count[26] = {0};
-    for(char c : magazine) count[c-'a']++;
-    for(char c : ransomNote) {
-        count[c-'a']--;
-        if(count[c-'a'] < 0) return false;
+    for(int i=0;i< magazine.length();i++) count[magazine[i]-'a']++;
+    for(int i=0;i<ransomNote.length();i++) {
+        count[ransomNote[i]-'a']--;
+        if(count[ransomNote[i]-'a'] < 0) return false;
     }
     return true;
+}
+
+bool binarySearch(int *arr, int s, int e , int k ) {
+
+    if(s>e)
+        return false;
+
+    int mid = s + (e-s)/2;
+
+    if(arr[mid] == k)
+        return true;
+
+    if(arr[mid] < k) {
+        return binarySearch(arr, mid+1, e, k);
+    }
+    else{
+        return binarySearch(arr, s, mid-1, k);
+    }
+}
+
+bool linearSearch(int arr[], int size, int k ) {
+   
+    if(size == 0) return false;
+
+    if(arr[0] == k) return true;
+    
+    else {
+        bool remainingPart = linearSearch(arr+1, size-1, k );
+        return remainingPart;
+    }
+}
+
+int getSum(int *arr, int size) {
+
+    if(size == 0)  return 0;
+
+    if(size == 1 ) return arr[0];
+
+    int remainingPart = getSum(arr+1, size-1);
+    int sum = arr[0] + remainingPart;
+    return sum;
+}
+
+bool isSorted(int arr[], int size) {
+
+    if(size == 0 || size == 1 )  return true;
+
+    if(arr[0] > arr[1])  return false;
+   
+    else {
+        bool remainingPart = isSorted(arr + 1, size - 1 );
+        return remainingPart;
+    }
 }
 
 #endif
